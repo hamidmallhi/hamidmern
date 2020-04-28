a47fb4b270606d0db172f3b4fb57837ca9abffd8
hamidmallhi <hamidansarullah@hotmail.com>
2020-04-28 17:34:55 -0600

    Second Commit

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    MONGODB_URI: 'mongodb+srv://hamidmallhi:Ansarullah1@cluster1-ihrsa.mongodb.net/test?retryWrites=true&w=majority'
+
+}
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+// require('dotenv').config()
 // Import npm packages
 const express = require('express');
 const mongoose = require('mongoose');
@@ -7,32 +7,46 @@ const path = require('path');
 
 
 const app = express();
-const PORT = process.env.PORT || 2020;   
+// const PORT = process.env.PORT || 2020;   
 
 const routes = require('./routes/articles');
 
-  mongoose.connect(process.env.MONGODB_URI, {
+// DB Config
+const db = require('./config/keys').MONGODB_URI
 
+// connect to MongoDB
+
+  mongoose
+  .connect(db, {
     useNewUrlParser: true,
     useUnifiedTopology: true
-});
+})
+  .then(() => console.log('MongoDB Connected!!!'))
+  .catch(err => console.log(err))  
 
-mongoose.connection.on('connected', () => {
-    console.log('Mongoose is connected!');
-});
+  const port = process.env.PORT || 5000
+
+
+    
+
+// mongoose.connection.on('connected', () => {
+//     console.log('Mongoose is connected!');
+// });
+
+
 
 // Data parsing
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // serve static assets if in production
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'))
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static('client/build'))
 
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    })
-}
+//     app.get('*', (req, res) => {
+//       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+//     })
+// }
 
 
 // HTTP request logger
@@ -41,5 +55,6 @@ app.use(morgan('tiny'));
 // Base Route
   app.use('/articles', routes);
 
-app.listen(PORT, console.log(`Server is starting at ${PORT}`));
+app.listen(port, console.log(`Server is starting at ${port}`));
+
 
